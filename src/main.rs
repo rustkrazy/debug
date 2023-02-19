30d4eefb41e66e793717a84d7f30729597b7526b
HimbeerserverDE <himbeerserverde@gmail.com>
2023-02-19 18:53:48 +0100

    write 'boot successful' to /debug.txt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let mut f = File::create("/debug.txt")?;
+    f.write_all("boot successful".as_bytes())?;
+
+    Ok(())
 }
