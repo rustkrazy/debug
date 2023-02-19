03148c95003d4ddd83e12481aca6e501170a0ea8
HimbeerserverDE <himbeerserverde@gmail.com>
2023-02-19 19:05:10 +0100

    mount boot partition and place debug file there

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use std::fs::File;
 use std::io::{self, Write};
 
+use sys_mount::{Mount, Unmount, UnmountFlags};
+
 fn main() -> io::Result<()> {
-    let mut f = File::create("/debug.txt")?;
+    let _mount = Mount::builder()
+        .fstype("fat")
+        .mount("/dev/disk/by-partuuid/00000000-01", "/boot")
+        .expect("can't mount boot partition");
+
+    let _mount = _mount.into_unmount_drop(UnmountFlags::DETACH);
+
+    let mut f = File::create("/boot/debug.txt")?;
     f.write_all("boot successful".as_bytes())?;
 
     Ok(())
