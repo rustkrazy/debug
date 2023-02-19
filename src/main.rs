3d854b029023287ba78c0b59fd08ad0e16ba903e
HimbeerserverDE <himbeerserverde@gmail.com>
2023-02-19 19:14:20 +0100

    mount /dev/mmcblk0p1

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use sys_mount::{Mount, Unmount, UnmountFlags};
 fn main() -> io::Result<()> {
     let _mount = Mount::builder()
         .fstype("fat")
-        .mount("/dev/disk/by-partuuid/00000000-01", "/boot")
+        .mount("/dev/mmcblk0p1", "/boot")
         .expect("can't mount boot partition");
 
     let _mount = _mount.into_unmount_drop(UnmountFlags::DETACH);
